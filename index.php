376f92394688e6c67ee8d152777b3b9443494c1c
justin <harwoodjp@g.cofc.edu>
2016-11-22 11:25:41 -0500

    viewport tweak

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 ?>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=.65">
 	<link rel="stylesheet" href="styles/normalize.css">
 	<link rel="stylesheet" href="styles/style.css">
 	<link href="https://fonts.googleapis.com/css?family=Dosis" rel="stylesheet">
